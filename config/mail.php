fc582d6233ccc9a754e0d19f614b30492e7a823d
wriothesley-XD <12345678+wriothesley-XD@users.noreply.github.com>
2026-07-28 14:45:57 +0700

    gweh kerja

--- config/mail.php
+++ config/mail.php
@@ -114,5 +114,6 @@ return [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', env('APP_NAME', 'Laravel')),
     ],
+    'fsi_official_email' => env('FSI_OFFICIAL_EMAIL'),
 
 ];

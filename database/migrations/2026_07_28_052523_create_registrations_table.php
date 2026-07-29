9f6c753df11dd07b536375a4311dc98f2fa32e5f
kekes-c <khairunnisazahirasamsung@gmail.com>
2026-07-29 10:01:27 +0700

    regis kirik

---
+++ database/migrations/2026_07_28_052523_create_registrations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registrations', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('nickname');
+            $table->string('class');
+            $table->string('username_ig')->unique();
+            $table->string('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registrations');
+    }
+};

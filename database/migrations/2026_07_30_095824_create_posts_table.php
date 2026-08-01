104b896f6ee5f90ca5db065e70d3d0695dfa6084
kekes-c <khairunnisazahirasamsung@gmail.com>
2026-08-01 08:25:18 +0700

    kes

--- database/migrations/2026_07_30_095824_create_posts_table.php
+++ database/migrations/2026_07_30_095824_create_posts_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('author');
-            $table->string('cover_image')->nullable();
-            $table->string('pdf_path')->nullable();
-            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('posts');
     }
 };
